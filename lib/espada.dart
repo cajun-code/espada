409809f2a72e6af681aa0b1e51db0fe20291afeb
Allan Davis <alley@RedLion.attlocal.net>
2020-10-14 21:28:08 -0400

    added version handler

--- lib/espada.dart
+++ lib/espada.dart
@@ -4,13 +4,18 @@ import 'package:espada/commands/source_command.dart';
 import './commands/project_command.dart';
 import './commands/class_command.dart';
 
+const String VERSION = '3.0.1';
+
 class Espada {
   static void run(List<String> args) {
-    var runner = CommandRunner('espada', 'Tools for C++ Development');
+    var runner = CommandRunner('espada', 'Project tools for C++ Development');
     runner.addCommand(ProjectCommand());
     runner.addCommand(ClassCommand());
     runner.addCommand(HeaderCommand());
     runner.addCommand(SourceCommand());
-    runner.run(args);
+    runner.run(args).catchError((error) {
+      print('Espada: v$VERSION');
+      runner.printUsage();
+    });
   }
 }
